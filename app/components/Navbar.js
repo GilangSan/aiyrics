a14f6bd45c372c3752b30830c2bcc837d30e75fa
GilangSan <gilangsandi823@gmail.com>
2025-11-10 17:20:35 +0700

    add icon to navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Github } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 export const Navbar = () => {
@@ -13,8 +14,8 @@ export const Navbar = () => {
     <div className="w-full py-4 px-8 flex justify-between items-center backdrop-blur-md">
       <span className="text-xl font-bold display">AIyrics</span>
       <div className="flex gap-5">
-        <Button variant="outline" onClick={handleDirect} size="lg">
-          Github
+        <Button variant="outline" className={'cursor-pointer'} onClick={handleDirect} size="lg">
+          <Github />
         </Button>
       </div>
     </div>
